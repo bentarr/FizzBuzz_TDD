0b1a15b7731126240de1ebac933e0fac8eb8e671
bentarr <benetierpaul@gmail.com>
2021-03-27 21:27:50 +0100

    create main.py to launch the FizzBuzz game
    on a aussi rajouté une boucle while pour que on puisse jouer plusieurs fois d'affilée

---
+++ main.py
@@ -0,0 +1,13 @@
+import fizzbuzz
+
+def main():
+    print()
+    print("FizzBuzz game")
+    print()
+
+    GameOn = True
+    while GameOn:
+        fizzbuzz.AliceNumber()
+
+if __name__ == '__main__':
+  main()
