02ce39661b5b7e84f120a946f2e262fb726380f0
bentarr <benetierpaul@gmail.com>
2021-03-27 18:12:31 +0100

    second test but error on negative

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -5,3 +5,7 @@ class fizzbuzz_test(unittest.TestCase):
     def test_returns_error_with_number_0(self):
         with self.assertRaises(ValueError):
             FizzBuzz(0)
+
+    def test_returns_error_with_ngative_number(self):
+        with self.assertRaises(ValueError):
+            FizzBuzz(-15)
